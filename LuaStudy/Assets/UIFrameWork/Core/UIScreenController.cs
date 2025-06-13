2d38c11df3a8cdd33705dafae58a2d8a3ff7cb3f
aGeniusX <103927601+aGeniusX@users.noreply.github.com>
2025-06-13 18:11:46 +0800

    更新UIScreenController和FadeAni

--- LuaStudy/Assets/UIFrameWork/Core/UIScreenController.cs
+++ LuaStudy/Assets/UIFrameWork/Core/UIScreenController.cs
@@ -7,22 +7,210 @@ using UnityEngine;
 /// <typeparam name="TProps"></typeparam>
 public abstract class UIScreenController<TProps> : MonoBehaviour, IScreenController where TProps : IScreenProperties
 {
-    public string ScreenId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    [Header("Screen Animations")]
+    [Tooltip("界面显示的动画")]
+    [SerializeField]
+    /// <summary>
+    /// 界面显示的动画
+    /// </summary>
+    private AniComponment animIn;
+    [Tooltip("界面隐藏的动画")]
+    [SerializeField]
+    /// <summary>
+    /// 界面隐藏的动画
+    /// </summary>
+    private AniComponment animOut;
 
-    public bool IsVisible => throw new NotImplementedException();
+    [Header("Screen Properties")]
+    [Tooltip("界面的属性参数")]
+    [SerializeField]
+    /// <summary>
+    /// 界面的属性参数
+    /// </summary>
+    private TProps properties;
 
-    public Action<IScreenController> InTransitionFinished { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public Action<IScreenController> OutTransitionFinished { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public Action<IScreenController> CloseRequest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public Action<IScreenController> ScreenDestroyed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    /// <summary>
+    /// 界面ID
+    /// </summary>
+    /// <value></value>
+    public string ScreenId { get; set; }
 
+    /// <summary>
+    /// 动画组件，为了界面有统一的弹出效果
+    /// </summary>
+    /// <value></value>
+    public AniComponment AnimIn
+    {
+        get { return animIn; }
+        set { animIn = value; }
+    }
+    /// <summary>
+    /// 动画组件，为了界面有统一的隐藏效果
+    /// </summary>
+    /// <value></value>
+    public AniComponment AnimOut
+    {
+        get { return animOut; }
+        set { animOut = value; }
+    }
+
+    /// <summary>
+    /// 界面是否显示中
+    /// </summary>
+    /// <value></value>
+    public bool IsVisible { get; set; }
+
+    /// <summary>
+    /// 弹出（渐入）动画完成之后回调
+    /// </summary>
+    /// <value></value>
+    public Action<IScreenController> InTransitionFinished { get; set; }
+    /// <summary>
+    /// 关闭（渐隐）动画完成之后回调
+    /// </summary>
+    /// <value></value>
+    public Action<IScreenController> OutTransitionFinished { get; set; }
+    /// <summary>
+    /// 关闭界面的回调
+    /// </summary>
+    /// <value></value>
+    public Action<IScreenController> CloseRequest { get; set; }
+    /// <summary>
+    /// 界面销毁的回调
+    /// </summary>
+    /// <value></value>
+    public Action<IScreenController> ScreenDestroyed { get; set; }
+
+    public TProps Properties
+    {
+        get { return properties; }
+        set { properties = value; }
+    }
+
+    protected virtual void Awake()
+    {
+        AddListeners();
+    }
+
+
+    protected virtual void OnDestroy()
+    {
+        if (ScreenDestroyed != null)
+        {
+            ScreenDestroyed(this);
+        }
+        InTransitionFinished = null;
+        OutTransitionFinished = null;
+        CloseRequest = null;
+        ScreenDestroyed = null;
+        RemoveListeners();
+    }
+
+    /// <summary>
+    /// 添加监听事件，awake是注册
+    /// </summary>
+    protected virtual void AddListeners() { }
+    /// <summary>
+    /// 移除监听事件，Destroy会自动调
+    /// </summary>
+    protected virtual void RemoveListeners() { }
+
+    /// <summary>
+    /// 属性参数设置到界面的时候触发，在SetProperties之后触发，比较安全的能取到值
+    /// </summary>
+    protected virtual void OnPropertiesSet() { }
+    /// <summary>
+    /// 界面隐藏的时候触发，便于处理一些操作
+    /// </summary>
+    protected virtual void WhileHiding() { }
+
+    /// <summary>
+    /// 设置属性参考
+    /// </summary>
+    /// <param name="props"></param>
+    protected virtual void SetProperties(TProps props) { }
+
+    /// <summary>
+    /// 在显示的时候处理一些层级，或者属性处理等，具体看继承者重写了
+    /// </summary>
+    protected virtual void HierarchyFixOnShow() { }
+
+    /// <summary>
+    /// 隐藏界面
+    /// </summary>
+    /// <param name="animate"></param>
     public void Hide(bool animate = true)
     {
-        throw new NotImplementedException();
+        DoAnimation(animate ? animOut : null, OnTransitionOutFinished, false);
     }
 
+
+    /// <summary>
+    /// 显示界面
+    /// </summary>
+    /// <param name="props"></param>
     public void Show(IScreenProperties props = null)
     {
-        throw new NotImplementedException();
+        if (props != null)
+        {
+            if (props is TProps)
+                SetProperties((TProps)props);
+            else
+            {
+                Debug.LogError("Properties passed have wrong type! (" + props.GetType() + " instead of " +
+                                   typeof(TProps) + ")");
+                return;
+            }
+        }
+        HierarchyFixOnShow();
+        OnPropertiesSet();
+        if (!gameObject.activeSelf)
+        {
+            DoAnimation(animIn, OnTransitionInFinished, true);
+        }
+    }
+
+    private void OnTransitionInFinished()
+    {
+        IsVisible = true;
+
+        if (InTransitionFinished != null)
+        {
+            InTransitionFinished(this);
+        }
+    }
+
+    /// <summary>
+    /// 完成过渡后调用
+    /// </summary>
+    private void OnTransitionOutFinished()
+    {
+        IsVisible = false;
+        if (OutTransitionFinished != null)
+        {
+            OutTransitionFinished(this);
+        }
+    }
+    /// <summary>
+    /// 播放ui动画
+    /// </summary>
+    /// <param name="caller"></param>
+    /// <param name="callWhenFinished"></param>
+    /// <param name="isVisible"></param>
+    private void DoAnimation(AniComponment caller, Action callWhenFinished, bool isVisible)
+    {
+        if (caller == null)
+        {
+            gameObject.SetActive(isVisible);
+            if (callWhenFinished != null)
+                callWhenFinished();
+        }
+        else
+        {
+            if (isVisible && gameObject.activeSelf)
+                gameObject.SetActive(true);
+            caller.Animate(transform, callWhenFinished);
+        }
     }
 }
+
--- LuaStudy/Assets/UIFrameWork/ViewAnimation/FadeAni.cs
+++ LuaStudy/Assets/UIFrameWork/ViewAnimation/FadeAni.cs
@@ -14,20 +14,77 @@ public class FadeAni : AniComponment
     private float fadeDuration = 0.5f;
     [SerializeField]
     /// <summary>
-    /// 是否进行隐去
+    /// 是否进行隐去,关闭窗口之类的
     /// </summary>
     private bool fadeOut = false;
 
     private CanvasGroup canvasGroups;
     private float timer;
-    private Action currentActrion;
+    /// <summary>
+    /// 当前行为
+    /// </summary>
+    private Action currentAction;
     private Transform currentTarget;
+    /// <summary>
+    /// 透明度开始值
+    /// </summary>
     private float startValue;
+    /// <summary>
+    /// 透明度结束值
+    /// </summary>
     private float endValue;
 
     private bool shouldAnimate;
     public override void Animate(Transform target, Action callWhenFinished)
     {
-        
+        if (currentAction != null)
+        {
+            canvasGroups.alpha = endValue;
+            currentAction();
+        }
+
+        canvasGroups = target.GetComponent<CanvasGroup>();
+        if (canvasGroups == null)
+        {
+            canvasGroups = target.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        if (fadeOut)
+        {
+            startValue = 1f;
+            endValue = 0;
+        }
+        else
+        {
+            startValue = 0f;
+            endValue = 1f;
+        }
+
+        currentAction = callWhenFinished;
+        timer = fadeDuration;
+
+        canvasGroups.alpha = startValue;
+        shouldAnimate = true;
+    }
+
+    void Update()
+    {
+        if (!shouldAnimate)
+            return;
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            canvasGroups.alpha = Mathf.Lerp(endValue, startValue, timer / fadeDuration);
+        }
+        else
+        {
+            canvasGroups.alpha = 1f;
+            if (currentAction != null)
+            {
+                currentAction();
+            }
+            currentAction = null;
+            shouldAnimate = true;
+        }
     }
 }
