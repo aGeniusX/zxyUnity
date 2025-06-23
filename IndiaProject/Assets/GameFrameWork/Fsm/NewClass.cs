67e2323fc5877fbba7ce586e0e036c5c1bf6fae9
aGeniusX <103927601+aGeniusX@users.noreply.github.com>
2025-06-23 17:32:42 +0800

    游戏框架更新

---
+++ IndiaProject/Assets/GameFrameWork/Fsm/NewClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndiaProject.Assets.GameFrameWork.Fsm
+{
+    public class NewClass
+    {
+        
+    }
+}
