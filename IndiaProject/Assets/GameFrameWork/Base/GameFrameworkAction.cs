a4063c3f6580ad9da2b138e6457fe971e1a66221
aGeniusX <103927601+aGeniusX@users.noreply.github.com>
2025-06-24 11:44:51 +0800

    游戏框架更新

---
+++ IndiaProject/Assets/GameFrameWork/Base/GameFrameworkAction.cs
@@ -0,0 +1,374 @@
+namespace GameFrameWork
+{
+    /// <summary>
+    /// 封装一个方法。该方法不具有参数并且不返回值
+    /// </summary>
+    public delegate void GameFrameworkAction();
+
+    /// <summary>
+    /// 封装一个方法，该方法只有一个参数并且不返回值
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T>(T obj);
+
+    /// <summary>
+    /// 封装一个方法，该方法只有两个参数并且不返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2>(T1 arg1, T2 arg2);
+
+    /// <summary>
+    /// 封装一个方法，该方法只有三个参数并且不返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+
+    /// <summary>
+    /// 封装一个方法，该方法有四个参数并且不返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+
+    /// <summary>
+    /// 封装一个方法，该方法有五个参数并且不返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+
+    /// <summary>
+    /// 封装一个方法，该方法有六个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+
+    /// <summary>
+    /// 封装一个方法，该方法有七个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+
+    /// <summary>
+    /// 封装一个方法，该方法有八个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
+
+    /// <summary>
+    /// 封装一个方法，该方法有九个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9);
+    /// <summary>
+    /// 封装一个方法，该方法有十个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10);
+
+    /// <summary>
+    /// 封装一个方法，该方法有十一个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11);
+
+    /// <summary>
+    /// 封装一个方法，该方法有十二个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <param name="arg12"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11, in T12>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12);
+
+    /// <summary>
+    /// 封装一个方法，该方法有十三个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <param name="arg12"></param>
+    /// <param name="arg13"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11, in T12, in T13>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13);
+
+    /// <summary>
+    /// 封装一个方法，该方法有十四个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <param name="arg12"></param>
+    /// <param name="arg13"></param>
+    /// <param name="arg14"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11, in T12, in T13, in T14>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14);
+
+    /// <summary>
+    /// 封装一个方法，该方法具有十五个参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <param name="arg12"></param>
+    /// <param name="arg13"></param>
+    /// <param name="arg14"></param>
+    /// <param name="arg15"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11, in T12, in T13, in T14, in T15>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15);
+
+    /// <summary>
+    /// 封装一个方法，该方法具有十六参数不具有返回值
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    /// <param name="arg3"></param>
+    /// <param name="arg4"></param>
+    /// <param name="arg5"></param>
+    /// <param name="arg6"></param>
+    /// <param name="arg7"></param>
+    /// <param name="arg8"></param>
+    /// <param name="arg9"></param>
+    /// <param name="arg10"></param>
+    /// <param name="arg11"></param>
+    /// <param name="arg12"></param>
+    /// <param name="arg13"></param>
+    /// <param name="arg14"></param>
+    /// <param name="arg15"></param>
+    /// <param name="arg16"></param>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    /// <typeparam name="T8"></typeparam>
+    /// <typeparam name="T9"></typeparam>
+    /// <typeparam name="T10"></typeparam>
+    /// <typeparam name="T11"></typeparam>
+    /// <typeparam name="T12"></typeparam>
+    /// <typeparam name="T13"></typeparam>
+    /// <typeparam name="T14"></typeparam>
+    /// <typeparam name="T15"></typeparam>
+    /// <typeparam name="T16"></typeparam>
+    /// <returns></returns>
+    public delegate void GameFrameworkAction<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8, in T9, in T10, in T11, in T12, in T13, in T14, in T15, in T16>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16);
+}
